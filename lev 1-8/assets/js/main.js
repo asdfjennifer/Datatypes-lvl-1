e0b87067148e2c7912e4523e74a3055576eff26b
asdfjennifer <jennifergenger@gmailcom>
2022-05-16 21:52:41 +0200

    Datatypes lvl 1

---
+++ lev 1-8/assets/js/main.js
@@ -0,0 +1,35 @@
+console.log("working?");
+
+// Deklariere die Variablen x = 20, y = 30.
+
+let x = 20;
+let y = 30;
+
+// Addiere x und y und gib das Ergebnis in der Konsole aus.
+
+console.log(x + y);
+
+// Subtrahiere y von x und zeige das Ergebnis in der Konsole. Dann subtrahiere x von y und gib das Ergebnis in der Konsole aus.
+
+console.log(x - y);
+console.log(y - x);
+// Multipliziere x und y und gib das Ergebnis der Konsole aus.
+
+console.log(x * y);
+// Teile x durch y und gib das Ergebnis mit der Konsole aus.
+
+console.log(x / y);
+// Deklariere eine weitere Variable z mit dem Wert 10. Multipliziere x und y. Teile dann das Ergebnis durch z. Speichere das Ergebnis in einer neuen Variablen namens resultOne.
+
+let z = 10;
+
+console.log(x * y);
+
+console.log(x * y / z);
+
+let resultOne = console.log(x * y / z).value;
+
+console.log(resultOne);
+
+
+
---
+++ lev 1_3/assets/js/main.js
@@ -0,0 +1,25 @@
+// VARIABLEN
+
+// let i = 1;         
+// let lastName = "Johnson";
+// let j = "2";
+// let status = true;
+// let hello = ”hello”;
+let hello = "hello";
+// let helloWorld = "hello";
+
+
+// console.log("working?");
+
+// console.log(typeof (i)); //number
+
+// console.log(typeof (lastName)); //string
+
+// console.log(typeof (j)); //string
+
+// console.log(typeof (status)); //string
+
+console.log(typeof (hello)); //undefined because of Anführungszeichen
+//string
+
+// console.log(typeof (helloWorld)); //string
